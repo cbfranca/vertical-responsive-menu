92c16a19e1eda77bd627da9811568ae222430ce1
cbfranca <cleidsonbf@gmail.com>
2014-09-01 20:31:29 -0300

    Initial commit - Including files in repository.

---
+++ responsive-left-menu.js
@@ -0,0 +1,21 @@
+/* =========================
+    TODO: 
+==========================*/
+
+var tid = setInterval( function () {
+	if ( document.readyState !== 'complete' ) return;
+	clearInterval( tid );           
+
+    document.getElementById('menuButton').addEventListener('click', function(e) {
+    	
+    	var nav = document.getElementById('verticalNav');
+    	var wrapper = document.getElementById('wrapper');
+
+    	nav.classList.toggle('toggle-menu');
+    	wrapper.classList.toggle('toggle-content');
+
+    	e.preventDefault();
+    });
+
+
+  }, 100 );
