2c63ee4cba8ec7ba513f88aeb8af66574e38fd7c
marcelo <marceloribeiromidia@gmail.com>
2015-03-02 18:59:04 -0300

    Commit parcial

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,17 +12,40 @@ module.exports = function(grunt) {
           src: ['*.scss'],
           dest: 'assets/css',
           ext: '.css'
-        }]
+        }],
+        options: {
+          lineNumbers: true,
+          sourcemap: 'none',
+          noCache: true
+        },
       }
+    },
+
+    watch: {
+      css: {
+        files: 'assets/sass/*.scss',
+        tasks: ['sass']
+        // tasks: ['sass', 'csscomb']
+      }
+    },
+
+    csscomb: {
+        dynamic_mappings: {
+            expand: true,
+            cwd: 'assets/css/',
+            src: ['*.css'],
+            dest: 'assets/css/',
+            ext: '.css'
+        }
     }
 
   });
 
   // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
-
+  grunt.loadNpmTasks('grunt-csscomb');
   // Default task(s).
-  grunt.registerTask('default', ['sass']);
-  grunt.registerTask('sass', ['sass']);
+  grunt.registerTask('default', ['watch']);
 
 };
